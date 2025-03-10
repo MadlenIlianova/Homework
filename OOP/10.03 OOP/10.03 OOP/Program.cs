f55bd14fcf6a10f1520e7282e65b27c28fcfab63
MadlenIlianova <madlenveselinova4@gmail.com>
2025-03-10 15:05:00 +0200

    123

---
+++ OOP/10.03 OOP/10.03 OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿Car car = new Car();
+Car car2 = new Car();
+Car car3 = new Car();
+Car car4 = new Car();
+
+public class Car
+{
+    public Car()
+    {
+        Id++;
+    }
+    public static int Id { get; private set; }
+    public void Test()
+    {
+
+    }
+}
+
