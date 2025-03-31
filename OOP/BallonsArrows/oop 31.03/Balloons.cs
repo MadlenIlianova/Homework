ceaa13ac23908179e3453b7c907ee3b097d61636
MadlenIlianova <madlenveselinova4@gmail.com>
2025-03-31 16:00:10 +0300

    BallonsArrows

---
+++ OOP/BallonsArrows/oop 31.03/Balloons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_31._03
+{
+    public class Balloons
+    {
+        public int BalloonSize { get; set; }
+
+        public Balloons()
+        {
+            Random random = new Random();
+            BalloonSize = random.Next(0, 101);
+        }
+    }
+}
