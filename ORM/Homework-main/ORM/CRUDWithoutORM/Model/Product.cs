df77baa17687afa68ddc55353c1385ef21edce8a
MadlenIlianova <madlenveselinova4@gmail.com>
2025-05-12 14:45:10 +0300

    Remove

--- ORM/Homework-main/ORM/CRUDWithoutORM/Model/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-
-    public List<Buyer>? Buyers { get; set; }
-}
