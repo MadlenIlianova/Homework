04c488f05ecb111140557d6286e49609e0d8748e
MadlenIlianova <madlenveselinova4@gmail.com>
2025-06-09 22:17:14 +0300

    .

---
+++ ORM/Electric_Vehicle_Population_Data/DataContext/VehicleService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContext
+{
+    public class VehicleService
+    {
+        private readonly Repository _repository;
+        private readonly VehicleDbContext _context;
+
+        public VehicleService(VehicleDbContext context)
+        {
+            _context = context;
+            _repository = new Repository(context);
+        }
+
+        public void DeleteVehicle(int id)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(v => v.Id == id && !v.IsDeletedAt);
+            if (vehicle != null)
+            {
+                _repository.SoftDelete(vehicle);
+            }
+        }
+    }
+
+}
